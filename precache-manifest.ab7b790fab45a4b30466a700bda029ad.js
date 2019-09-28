91d48e0a0eed6e8484748f4edc62f062796dbb57
beiatrix <bpedrasa@gmail.com>
2019-09-28 09:46:21 -0700

    Updates

---
+++ precache-manifest.ab7b790fab45a4b30466a700bda029ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f23926026e668dba595e5b755f5b24",
+    "url": "/countdown/index.html"
+  },
+  {
+    "revision": "fbbb0a25b67383a76b62",
+    "url": "/countdown/static/css/main.3d5bcf8c.chunk.css"
+  },
+  {
+    "revision": "018a905551ea0db00db6",
+    "url": "/countdown/static/js/2.69ca439c.chunk.js"
+  },
+  {
+    "revision": "fbbb0a25b67383a76b62",
+    "url": "/countdown/static/js/main.32e42630.chunk.js"
+  },
+  {
+    "revision": "4210fd0d9545e2133b78",
+    "url": "/countdown/static/js/runtime-main.f62a110f.js"
+  }
+]);
