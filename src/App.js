0711fc3756f513a1a8d802a5697bb02acbe61236
beiatrix <bpedrasa@gmail.com>
2019-10-03 19:24:06 -0700

    Add progress bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import moment from 'moment'
+import { Progress } from 'antd'
 
 export default () => {
   const one_year = moment("20200513")
@@ -12,6 +13,16 @@ export default () => {
         <code>Today is {now.format('dddd, MMMM Do YYYY')}</code>
         <h1>{one_year.diff(now, 'days')}</h1>
         <code>days until May 13, 2020</code>
+        <div id="progress">
+        <Progress
+          strokeColor={{
+            '0%': '#108ee9',
+            '100%': '#09d3ac',
+          }}
+          percent={Math.floor((365 - one_year.diff(now, 'days')) / 365 * 100)}
+          showInfo={false}
+        />
+        </div>
       </header>
     </div>
   );
